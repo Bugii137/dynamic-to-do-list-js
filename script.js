5f8a4a9a37a1252b651526d9fd5af5be919ef4e1
Mark Kabugi <kabug222@gmail.com>
2025-06-14 20:54:23 +0300

    script.js

--- script.js
+++ script.js
@@ -17,6 +17,7 @@ document.addEventListener('DOMContentLoaded', function () {
         const removeBtn = document.createElement('button');
         removeBtn.textContent = 'Remove';
         removeBtn.className = 'remove-btn';
+
         removeBtn.onclick = function () {
             taskList.removeChild(li);
         };
